c177cca28903fa69707edc921ad4d0fea59de2c1
MacAlsandair <mac.alsandair@gmail.com>
2023-04-24 18:34:52 +0200

    Fix the error with BookRepository

--- src/main/java/com/macalsandair/library/book/BookRepository.java
+++ src/main/java/com/macalsandair/library/book/BookRepository.java
@@ -2,6 +2,6 @@ package com.macalsandair.library.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
