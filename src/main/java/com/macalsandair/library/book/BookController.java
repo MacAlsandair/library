c8b68d0f3ab89e9d37c3c341a8f1607de709af02
MacAlsandair <mac.alsandair@gmail.com>
2023-04-24 18:45:41 +0200

    Add PostMapping annotation for addBook method

--- src/main/java/com/macalsandair/library/book/BookController.java
+++ src/main/java/com/macalsandair/library/book/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ public class BookController {
 		return new ResponseEntity<List<Book>>(books, HttpStatus.OK);
 	}
 	
+	@PostMapping("/add")
 	public ResponseEntity<Book> addBook(@RequestBody Book book) {
 		bookRepository.save(book);
 		return new ResponseEntity<Book>(book, HttpStatus.CREATED);
