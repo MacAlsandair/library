754a23ae8d3327cb2c5fb13ad027cd1fd0c4b05d
MacAlsandair <mac.alsandair@gmail.com>
2023-04-26 10:11:15 +0200

    Add findBookById method in BookRepository

--- src/main/java/com/macalsandair/library/book/BookRepository.java
+++ src/main/java/com/macalsandair/library/book/BookRepository.java
@@ -1,7 +1,10 @@
 package com.macalsandair.library.book;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+	Optional<Book> findBookById(Long id);
 }
