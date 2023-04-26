4281a92d4962c885c0b31d1992dfa8db55c7f04d
MacAlsandair <mac.alsandair@gmail.com>
2023-04-26 10:15:20 +0200

    Add findBookById method in BookService

--- src/main/java/com/macalsandair/library/book/BookService.java
+++ src/main/java/com/macalsandair/library/book/BookService.java
@@ -1,10 +1,16 @@
 package com.macalsandair.library.book;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 	
 	@Autowired
-	private
+	private BookRepository bookRepository;
+	
+	public Book findBookById (Long id) {
+		return bookRepository.findById(id)
+				.orElseThrow(() -> new BookNotFoundException("Book by id " + id + " was not found"));
+	}
 }
