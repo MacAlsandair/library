7ea5bc9936efbe9c19f0b34e05345bd8fa015ef9
MacAlsandair <mac.alsandair@gmail.com>
2023-06-29 10:06:54 +0200

    Add new method for BookRepository

--- src/main/java/com/macalsandair/library/book/BookRepository.java
+++ src/main/java/com/macalsandair/library/book/BookRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 	Optional<Book> findBookById(Long id);
+	
+	Optional<Book> findByTitleAndAuthor(String title, String author);
 }
