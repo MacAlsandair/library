5373c33b874174fa5b18d84184fe2758a9f7fad3
MacAlsandair <mac.alsandair@gmail.com>
2023-06-30 12:56:54 +0200

    Create a CommentToBookRepository

---
+++ src/main/java/com/macalsandair/library/comment/CommentToBookRepository.java
@@ -0,0 +1,17 @@
+package com.macalsandair.library.comment;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.macalsandair.library.book.Book;
+import com.macalsandair.library.user.User;
+
+@Repository
+public interface CommentToBookRepository extends JpaRepository<CommentToBook, Long> {
+	
+	public List<CommentToBook> findByAuthor(User author);
+	
+	public List<CommentToBook> findByBook(Book book);
+}
