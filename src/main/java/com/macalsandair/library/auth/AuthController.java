58898554dca0c7e9e79657843e2bc50fd4cb4b36
MacAlsandair <mac.alsandair@gmail.com>
2023-07-02 11:34:06 +0200

    Change request type of deleting user method to delete type

--- src/main/java/com/macalsandair/library/auth/AuthController.java
+++ src/main/java/com/macalsandair/library/auth/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -91,7 +92,7 @@ public class AuthController {
         }
     }
     
-    @PostMapping("/delete")
+    @DeleteMapping("/delete")
     public ResponseEntity<String> deleteUser(Authentication authentication){
         String username = authentication.getName();
         User user = userRepository.findByUsername(username).get();
