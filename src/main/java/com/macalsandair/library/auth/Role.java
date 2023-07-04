1a13c222cb0e0663d22ebd351c7b347f72af86e5
MacAlsandair <mac.alsandair@gmail.com>
2023-07-04 18:21:22 +0200

    Add role support for even a request with JWT Bearer

--- src/main/java/com/macalsandair/library/auth/Role.java
+++ src/main/java/com/macalsandair/library/auth/Role.java
@@ -8,8 +8,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import com.macalsandair.library.auth.Permission;
-
 public enum Role implements GrantedAuthority {
 
 	USER(Collections.emptySet()),
---
+++ src/main/java/com/macalsandair/library/configuration/CustomJwtGrantedAuthoritiesConverter.java
@@ -0,0 +1,22 @@
+package com.macalsandair.library.configuration;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+
+
+public class CustomJwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        return Arrays.stream(jwt.getClaimAsString("scope").split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
